14ae1b0e4022951387cab7467e1f6354ae486621
Tejaswa <sc20tar@leeds.ac.uk>
2024-11-15 16:07:06 +0000

    vertex calc done, face left

--- assignment/face2faceindex.cpp
+++ assignment/face2faceindex.cpp
@@ -5,6 +5,7 @@
 #include "face2faceindex.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int main(int argc, char *argv[])
 {
@@ -27,12 +28,41 @@ int main(int argc, char *argv[])
 
     std::cout<<"File opened successfully"<<std::endl;
 
-    // print the file
     std::string line;
-    while (std::getline(file, line))
+    
+    // read the first line
+    std::getline(file, line);
+
+    // store the number of triangles
+    int numTriangles = std::stoi(line);
+
+    // store the vertices
+    std::vector<Cartesian3> vertices;
+    std::vector<Cartesian3> faces;
+    for (int i = 0; i < numTriangles; i++)
     {
-        std::cout << line << std::endl;
+        std::getline(file, line);
+        std::istringstream iss(line);
+        float x, y, z;
+        iss >> x >> y >> z;
+        vertices.push_back(Cartesian3(x, y, z));
     }
+
+    // objName will be the file name without the extension and the path
+    std::string objNameStr = input_filename.substr(input_filename.find_last_of("/\\") + 1);
+    objNameStr = objNameStr.substr(0, objNameStr.find_last_of("."));
+
+    // create a TriangleMesh object
+    TriangleMesh mesh(objNameStr, numTriangles, vertices);
+
+    int check = mesh.saveFile();
+    if (check == 1)
+    {
+        std::cerr << "Error: could not save file" << std::endl;
+        return 1;
+    }
+    
+    std::cout << "File saved successfully" << std::endl;    
 
     return 0;
 }
--- assignment/face2faceindex.h
+++ assignment/face2faceindex.h
@@ -7,4 +7,85 @@
 
 #include "../triangle_renderer/Cartesian3.h"
 
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class TriangleMesh
+{
+    public:
+    std::string objName;
+    int numTriangles;
+    int numVertices;
+    int numFaces;
+
+    std::vector<Cartesian3> vertices;
+    std::vector<Cartesian3> faces;
+
+    // Default constructor for TriangleMesh
+    TriangleMesh()
+    {
+        this->numTriangles = 0;
+        this->numVertices = 0;
+    }
+
+    // Constructor for TriangleMesh
+    TriangleMesh(std::string objName, int numTriangles, std::vector<Cartesian3> vertices)
+    {
+        this->objName = objName;
+        this->numTriangles = numTriangles;
+        this->numVertices = vertices.size();
+        this->vertices = vertices;
+        this->numFaces = numTriangles;
+    }
+
+    // print the vertices in a file
+    void printObjectName()
+    {
+        std::cout << "Object Name: " << this->objName << std::endl; 
+    }
+
+    int saveFile()
+    {
+        std::string filename = "output/" + this->objName + ".face";
+
+        // create the output directory if it doesn't exist
+        std::string output_dir = "output";
+        std::string command = "mkdir -p " + output_dir;
+        system(command.c_str());
+
+        std::ofstream file(filename.c_str());
+        if (!file.is_open())
+        {
+            std::cerr << "Error: could not open file " << filename << std::endl;
+            return 1;
+        }
+
+        file << "# University of Leeds 2024-2025" << std::endl;
+        file << "# COMP 5893M Assignment 1" << std::endl;
+        file << "# Student Name: Tejaswa Rizyal" << std::endl;
+        file << "# Student Number: 201484983" << std::endl;
+        file << "#"<< std::endl;
+        file << "# Object Name: " << this->objName << std::endl;
+        file << "# Vertices: " << this->numVertices << std::endl;
+        file << "#" << std::endl;
+
+        for (int i = 0; i < this->numVertices; i++)
+        {
+            file << "Vertex " << i << " " << this->vertices[i].x << " " << this->vertices[i].y << " " << this->vertices[i].z << std::endl;
+        }
+
+        for (int i = 0; i < this->numFaces; i++)
+        {
+            file << "Face " << i << " " << i << std::endl;
+        }
+
+        file.close();
+        return 0;
+    }
+
+};
+
+
 #endif //FACE2FACEINDEX_H
